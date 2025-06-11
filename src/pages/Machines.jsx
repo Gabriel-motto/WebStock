ba384486aa21b0a05955449c2a6d115f9a49428c
gabrielmottocomesa <gabrielmottocomesa@gmail.com>
2025-06-11 10:56:49 +0000

    ignore updated fetching api prisma testing

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -1,7 +1,24 @@
-
+import { useEffect, useState } from "react";
 
 export default function MachinesPage() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            "http://10.176.99.23/Prisma/api/AssetTree/GetChildAssets?parentAsset=HP1&loadSpares=false&loadMainDoc=false"
+        )
+            .then((response) => response.json())
+            .then((json) => setData(json));
+    }, []);
+
     return (
-        <h1>maquinas</h1>
-    )
+        <>
+            {data.map((item, index) => (
+                <div className="content">
+                    <h1 key={index}>{item.parentAsset}</h1>
+                    <h2 key={index}>{item.entityKey}</h2>
+                </div>
+            ))}
+        </>
+    );
 }
