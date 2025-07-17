119b806560cda9b71127c1404e0495418b4bf96c
Gabriel-motto <buderball11@gmail.com>
2025-07-17 12:01:41 +0200

    forced light mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { Provider } from "./components/ui/provider"
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Provider } from "./components/ui/provider";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ColorModeProvider } from "./components/ui/color-mode";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider>
+            <ColorModeProvider forcedTheme="light">
+                <App />
+            </ColorModeProvider>
+        </Provider>
+    </React.StrictMode>
+);
